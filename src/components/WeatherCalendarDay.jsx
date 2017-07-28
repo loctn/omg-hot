664b3f4709bb75e040769d55804b1628016cfd43
Loc Nguyen <loc2000@gmail.com>
2017-07-28 14:13:57 -0700

    Add Skycons

--- src/components/WeatherCalendarDay.jsx
+++ src/components/WeatherCalendarDay.jsx
@@ -1,34 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 import P from 'prop-types'
 import CSSModules from 'react-css-modules'
 
 import { kelvinToFahrenheit, kelvinToCelsius } from '~/lib/temperature'
+import skyconsMap from './skyconsMap.json'
 import styles from './WeatherCalendarDay.scss'
 
 
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'Jun', 'July', 'August', 'September', 'October', 'November', 'December']
 const DAYS_OF_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-const WeatherCalendarDay = ({ styles, forecast, isToday }) => {
-  const date = new Date(forecast.dt * 1000)
-  const dayOfWeek = isToday ? 'Now' : DAYS_OF_WEEK[date.getDay()].substr(0, 3)
-  const dateText = MONTHS[date.getMonth()].substr(0, 3) + ' ' + date.getDate()
-  const high = kelvinToFahrenheit(isToday ? forecast.main.temp : forecast.temp.max).toFixed(0)
-  const low = isToday ? '' : kelvinToFahrenheit(forecast.temp.min).toFixed(0)
+class WeatherCalendarDay extends Component {
+  componentDidMount() {
+    const weather = this.props.forecast.weather[0]
+    const skyconsFunction = Skycons[skyconsMap[weather.icon] || skyconsMap[weather.id]]
+    if (!skyconsFunction) return
+    
+    this.skycons = new Skycons({ color: '#0af' })
+    this.skycons.set(this.canvas, skyconsFunction)
+    this.skycons.play()
+  }
 
-  return (
-    <div styleName="component" style={{
-      marginLeft: isToday ? (100 * date.getDay() / 7) + '%' : '0'
-    }}>
-      <div styleName="day-of-week">{dayOfWeek}</div>
-      <div styleName="date">{dateText}</div>
-      <img src={`http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`} />
-      <div styleName="high">{high}</div>
-      {(low || low === 0) &&
-        <div styleName="low">{low}</div>
-      }
-    </div>
-  )
+  componentWillUnmount() {
+    this.skycons.remove(this.canvas)
+  }
+
+  render() {
+    const forecast = this.props.forecast
+    const isToday = this.props.isToday
+    const date = new Date(forecast.dt * 1000)
+    const dayOfWeek = isToday ? 'Now' : DAYS_OF_WEEK[date.getDay()].substr(0, 3)
+    const dateText = MONTHS[date.getMonth()].substr(0, 3) + ' ' + date.getDate()
+    const high = kelvinToFahrenheit(isToday ? forecast.main.temp : forecast.temp.max).toFixed(0)
+    const low = isToday ? '' : kelvinToFahrenheit(forecast.temp.min).toFixed(0)
+
+    return (
+      <div styleName="component" style={{
+        marginLeft: isToday ? (100 * date.getDay() / 7) + '%' : '0'
+      }}>
+        <div styleName="day-of-week">{dayOfWeek}</div>
+        <div styleName="date">{dateText}</div>
+        <canvas styleName="icon" ref={canvas => this.canvas = canvas} width="40" height="40"></canvas>
+        <div styleName="high">{high}</div>
+        {(low || low === 0) &&
+          <div styleName="low">{low}</div>
+        }
+      </div>
+    )
+  }
 }
 
 
