6eb175fa4cb3ff5d65f2a915b67b29239f7e9980
Loc Nguyen <loc2000@gmail.com>
2017-07-27 21:25:52 -0700

    Add geocoding and routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import P from 'prop-types'
 import CSSModules from 'react-css-modules'
+import { withRouter } from 'react-router-dom'
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
 import styles from './App.scss'
 
 
-const App = ({ styles }) => (
-  <div styleName="component">
-    the quick brown fox jumps over the lazy dog
-  </div>
-)
+class App extends Component {
+  state = {
+    address: 'San Francisco, CA'
+  }
+
+  constructor(props) {
+    super(props)
+    this.onPlaceChange = address => this.setState({ address: address })
+  }
+  
+  getCoordinates(address) {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => {
+        this.props.history.push(encodeURIComponent(address).replace(/%20/g, '+'))
+      })
+      .catch(error => console.error('Error', error))
+  }
+
+  handlePlaceEnterKeyDown = () => {
+    this.getCoordinates(this.state.address)
+  }
+
+  handlePlaceSelect = (address) => {
+    this.onPlaceChange(address)
+    this.getCoordinates(address)
+  }
+
+  render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onPlaceChange,
+    }
+
+    return (
+      <div styleName="component">
+        <PlacesAutocomplete inputProps={inputProps}
+          onEnterKeyDown={this.handlePlaceEnterKeyDown}
+          onSelect={this.handlePlaceSelect}
+        />
+      </div>
+    )
+  }
+}
 
 
-export default CSSModules(App, styles)
+export default withRouter(CSSModules(App, styles))
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { HashRouter } from 'react-router-dom';
 
 import App from './components/App'
 
 
 render(
-  <App />,
+  <HashRouter>
+    <App />
+  </HashRouter>,
   document.getElementById('root')
 )
